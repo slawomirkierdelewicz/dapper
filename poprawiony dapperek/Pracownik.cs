5450a58bbf5267e122a2b0ab25811401155a7933
Slawomir Kierdelewicz <kierdello@wp.pl>
2019-03-30 22:04:07 +0100

    a

---
+++ poprawiony dapperek/Pracownik.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Dapper;
+using System.Data.SqlClient;
+
+namespace poprawiony_dapperek
+{
+   public class Pracownik
+    {
+        
+        public int id { get; set; }
+        public string imie { get; set; }
+        public string nazwisko { get;  set; }
+        
+    }
+}
--- poprawiony dapperek/Program.cs
+++ poprawiony dapperek/Program.cs
@@ -3,13 +3,54 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data.SqlClient;
+using Dapper;
 
-namespace poprawiony_dapperek
+namespace poprawiony_dapperek 
 {
     class Program
     {
         static void Main(string[] args)
         {
+            using (var connection = new SqlConnection("Data Source=LENOVO-PC\\SQLEXPRESS;Initial Catalog=uzytkownicy;Integrated Security=True"))
+            {
+
+                //connection.Execute("INSERT INTO dbo.pracownicy (imie, nazwisko) Values(@imie, @nazwisko)", new[] {
+                //  new {imie = "lidka", nazwisko="drobisz" },
+                //new { imie = "mania", nazwisko="kupinska" },
+                //new { imie = "nina", nazwisko="kupinska" }
+                //});
+                string sql = "delete dbo.pracownicy where id=@id and nazwisko=@nazwisko";
+                //connection.Execute("update dbo.pracownicy set imie=@imie where id=@id ", new { imie = "mirek", id = 25 });
+
+                //connection.Execute(sql, new { id = 21, nazwisko = "rafalska" });
+                //string sql1 = "select * from dbo.Pracownicy where nazwisko=@nazwisko";
+
+                //var pracownicy = connection.Query<Pracownik>(sql1, new { nazwisko = "nowak" }).ToList();
+
+                //connection.Execute("INSERT INTO dbo.pracownicy (imie, nazwisko) Values(@imie, @nazwisko)", new { imie = "zosia", nazwisko = "kupinska" });
+                var pracownicy = connection.Query<Pracownik>("select * from dbo.Pracownicy").ToList();
+                //var pracownicy = connection.QueryFirst("select * from dbo.Pracownicy where imie=@imie", new { imie = "wacek" });
+                //var pracownicy = connection.Query<Pracownik>("wyswietl", new { imie = "lidka" }, commandType:System.Data.CommandType.StoredProcedure).ToList();
+                //Console.WriteLine(pracownicy);
+                foreach (var worker in pracownicy)
+                {
+                    Console.WriteLine(worker.id + " " + worker.imie + " " + worker.nazwisko);
+                }
+
+
+            }
+            Console.ReadKey();
         }
+
+       
+
+
     }
+    
+
+
+
 }
+
+
